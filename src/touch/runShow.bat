07df505260cdc1aaf51dc8a8a5e13cd173f9461a
Michela Ledwidge <michela@modprods.com>
2017-09-08 13:49:09 +1000

    batch script

---
+++ src/touch/runShow.bat
@@ -0,0 +1,15 @@
+@echo off
+
+set APPPATH="C:\Program Files\Derivative\TouchDesigner099\bin"
+set APP="TouchDesigner099.exe"
+set root="D:\ResilioSync\moodLabVisuals\src\touch"
+set project="moodlab.toe"
+
+set TOUCH_TEXT_CONSOLE=0
+
+# APP SPECIFIC
+set WIDTH=1920
+set HEIGHT=1080
+set DEBUG=0
+
+start %APPPATH%\%APP% "%root%\moodlab.toe"
